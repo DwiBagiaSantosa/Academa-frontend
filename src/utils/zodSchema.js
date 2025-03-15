9de51fce3e17ab1be4118af76bc7c155ce44e7e7
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2025-03-15 23:41:56 +0700

    implement with signin api

--- src/utils/zodSchema.js
+++ src/utils/zodSchema.js
@@ -1,7 +1,9 @@
 import { z } from "zod"
 
 export const signUpSchema = z.object({
-    name: z.string().min(5),
-    email: z.string().email(),
-    password: z.string().min(8),
+    name: z.string().nonempty("Name is required").min(5, "Name must be at least 5 characters long"),
+    email: z.string().nonempty("Email is required").email(),
+    password: z.string().nonempty("Password is required").min(8, "Password must be at least 8 characters long"),
 })
+
+export const signInSchema = signUpSchema.omit({ name: true })
