a50597f1694599e4978fb56e41e67942e884aa7d
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2025-03-22 21:09:00 +0700

    integrate api delete courses

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -25,3 +25,6 @@ export const updateCourse = async (data, id) =>
       },
     })
     .then((res) => res.data);
+
+export const deleteCourse = async (id) =>
+  apiInstanceAuth.delete(`/courses/${id}`).then((res) => res.data);
