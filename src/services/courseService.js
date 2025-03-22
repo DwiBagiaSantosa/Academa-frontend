e4a57111f481857f0b85eedb2a0f15f25f14ecd3
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2025-03-22 20:02:59 +0700

    integrate api create courses

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -1,4 +1,14 @@
-import { apiInstanceAuth } from "../utils/axios"
+import { apiInstanceAuth } from "../utils/axios";
 
-export const getCourses = async () => apiInstanceAuth.get('/courses').then((res) => res.data)
-export const getCategories = async () => apiInstanceAuth.get('/categories').then((res) => res.data)
+export const getCourses = async () =>
+  apiInstanceAuth.get("/courses").then((res) => res.data);
+export const getCategories = async () =>
+  apiInstanceAuth.get("/categories").then((res) => res.data);
+export const createCourse = async (data) =>
+  apiInstanceAuth
+    .post("/courses", data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    .then((res) => res.data);
